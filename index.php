ac2a6363fadcd2041714c1c9eb19a141ca21b5bd
Joel Domingues <me@joeldomingues.com>
2023-12-14 16:56:33 +0000

    Testing with getID3 to obtain file, trying to adjust playing speed

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ require __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+$getID3 = new getID3;
+
 $serverIp = $_ENV['SERVER_IP'];
 $serverPort = $_ENV['SERVER_PORT'];
 $serverMountPoint = $_ENV['SERVER_MOUNT_POINT'];
@@ -13,12 +15,18 @@ $username = $_ENV['USERNAME'];
 $password = $_ENV['PASSWORD'];
 $filePath = $_ENV['FILE_PATH'];
 
+// Get the bitrate of the MP3 file
+$file = $getID3->analyze($filePath);
+$bitrate = $file['audio']['bitrate'];
+
 if ($serverIp == null || $serverPort == null || $serverMountPoint == null || $username == null || $password == null || $filePath == null) {
     die('Please set all environment variables.');
 }
 
 // Open the MP3 file for reading
 $fileHandle = fopen($filePath, 'r');
+$fileSize = filesize($filePath);
+$chunkSize = ($bitrate * 1024) / 8; // size of each chunk in bytes
 
 if (!$fileHandle) {
     die('Unable to open the MP3 file.');
@@ -41,7 +49,8 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     'Ice-Name: ' . 'FT-033',
     'Ice-Description: ' . '',
     'Ice-URL: ' . 'https://radioparanoia1000.com',
-    'Connection: Keep-Alive'
+    'Connection: Keep-Alive',
+    'Content-Length: ' . $fileSize,
 ));
 
 curl_setopt($ch, CURLOPT_VERBOSE, true);
@@ -53,9 +62,12 @@ curl_setopt($ch, CURLOPT_PUT, true);
 // Tell cURL that we want to send the contents of $fileHandle to the server
 curl_setopt($ch, CURLOPT_INFILE, $fileHandle);
 
-// We're going to "PUT" this information
-curl_setopt($ch, CURLOPT_INFILESIZE, filesize($filePath));
 
+// We're going to "PUT" this information
+// curl_setopt($ch, CURLOPT_INFILESIZE, filesize($filePath));
+curl_setopt($ch, CURLOPT_INFILESIZE, $chunkSize);
+
+/*
 // Execute cURL session
 $response = curl_exec($ch);
 
@@ -73,5 +85,17 @@ curl_close($ch);
 
 // Close the MP3 file handle
 fclose($fileHandle);
+*/
 
+while (!feof($fileHandle)) {
+    $data = fread($fileHandle, $chunkSize);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_exec($ch);
+    
+    // Wait for 1 second
+    sleep(1);
+}
+
+fclose($fileHandle);
+curl_close($ch);
 ?>
